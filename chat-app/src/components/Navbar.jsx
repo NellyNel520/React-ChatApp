23ccf349a29ecf5731b272e9c0d5fed2be3bfbc7
NellyNel520 <channelharris520@gmail.com>
2023-08-15 17:50:40 -0400

    added signout function from firebase to logout button within nav bar. successful

--- chat-app/src/components/Navbar.jsx
+++ chat-app/src/components/Navbar.jsx
@@ -1,16 +1,21 @@
+import { signOut } from 'firebase/auth'
+import { auth } from '../firebase'
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-      <span className='logo'>ChatterBox</span>
-      <div className='user'>
-        <img src="https://images.unsplash.com/photo-1691860305089-9a2566296202?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60" alt="cake" />
-        <span>Nelly</span>
-        <button>Logout</button>
-      </div>
-    </div>
-  )
+	return (
+		<div className="navbar">
+			<span className="logo">ChatterBox</span>
+			<div className="user">
+				<img
+					src="https://images.unsplash.com/photo-1691860305089-9a2566296202?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"
+					alt="cake"
+				/>
+				<span>Nelly</span>
+				<button onClick={() => signOut(auth)}>Logout</button>
+			</div>
+		</div>
+	)
 }
 
 export default Navbar
