604d5793a3d676e06571f94fa695b1f5be9fde0e
NellyNel520 <channelharris520@gmail.com>
2023-08-14 21:58:24 -0400

    added responsive design with sass

--- chat-app/src/components/Input.jsx
+++ chat-app/src/components/Input.jsx
@@ -5,7 +5,7 @@ const Input = () => {
     <div className='input'>
       <input type='text' placeholder='Type something...' />
       <div className='send'>
-        <img src='https://img.icons8.com/ios-filled/50/attach.png' alt='' />
+        <img src='https://img.icons8.com/material-sharp/24/attach.png' alt='' />
         <input type='file' style={{display: 'none'}} id='file'/>
         <label htmlFor='file'>
           <img src='https://img.icons8.com/dotty/80/add-image.png' alt='' />
--- chat-app/src/components/Message.jsx
+++ chat-app/src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className='messageInfo'>
+        <img src='https://images.unsplash.com/photo-1691495258003-aa698004a721?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1NHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60' alt='' />
+        <span>just now</span>
+      </div>
+      <div className='messageContent'>
+        <p>hello</p>
+        <img src='https://images.unsplash.com/photo-1691944435602-a219037ac417?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2OHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60' alt='' />
+      </div>
+    </div>
   )
 }
 
